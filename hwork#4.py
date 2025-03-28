42f46f462c49d62c607158fcc6ca54aa781da99c
wistykk <ilaivanus497@gmail.com>
2025-03-28 22:15:22 +0200

    homework4

--- hwork#4.py
+++ hwork#4.py
@@ -26,4 +26,43 @@ cart=Cart()
 cart.add(p1, p2)
 cart.details()
 print("вся сума:", cart.all_price())
+#дз2
+class BankAccount:
+    def __init__(self,owner,number,balance):
+        self.owner=owner
+        self.number=number
+        self.balance=balance
+    def info(self):
+        return 'владелец:' + self.owner + ' баланс:' + str(self.balance)
+    def deposit(self, amount): #внести
+        self.balance += amount
+    def withdraw(self, amount): #снятие
+        if self.balance >= amount:
+            self.balance -= amount
+        else:
+            print("недостаточно денег на счету")
 
+class Bank:
+    def __init__(self):
+        self.accounts = []
+    def add_account(self, account):
+        self.accounts.append(account)
+    def transfer(self, from_acc, to_acc, amount):
+        if from_acc.balance >= amount:
+            from_acc.balance -= amount
+            to_acc.balance += amount
+        else:
+            print("недостаточно денег на перевод")
+    def show_accounts(self):
+        for acc in self.accounts:
+            print(acc.info())
+
+acc1 = BankAccount("Олег", "12345", 0)
+acc2 = BankAccount("Марія", "67890", 400)
+bank = Bank()
+bank.add_account(acc1)
+bank.add_account(acc2)
+acc1.deposit(2000)
+bank.transfer(acc1, acc2, 100)
+acc2.withdraw(500)
+bank.show_accounts()
